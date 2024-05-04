9f8b9dfa0913f8f0ecdd8bfe545e47206d8041e7
github_KinmatTech <mathew.ani@unn.edu.ng>
2024-05-04 02:54:04 +0100

    https://github.com/KinmatTech/Kinmat-Tech-blog.git

---
+++ includes/header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">  
+
+  <head>
+
+  <link rel="shortcut icon" href="favicon.ico" type="image/png">
+        <meta name="keywords" content="News, trending, media, magazine, gossip, celebrities, lessons ,references,examples,tips, videos, beginners, free">
+        <meta property="og:image" content="#">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <meta name="author" content="mathewok20@gmail.com" >
+        <meta name="theme-color" content="#ffffff">
+        <link rel="stylesheet" href="style.css">
+        <meta name="msapplication-navbutton-color" content="#ffffff">
+        <meta name="apple-mobile-web-app-status-bar-style" content="#ffffff">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   
+
+    <title>Kinmat Technologies Limited | Blog Detail</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+ <style>
+    /*header*/
+    .brand{
+    text-align: center;
+     float: end;
+     margin-top: -30px;
+  }
+  .brand a{
+      text-decoration: none;
+      color: #fff;
+      font-family: Berlin Sans FB;
+      -webkit-animation-name: example; /* Chrome, Safari, Opera */
+      -webkit-animation-duration: 4s; /* Chrome, Safari, Opera */
+      animation-name: branddesign;
+      animation-duration: 4s;
+      animation-iteration-count: 2;
+  }
+  </style>
+ <nav class="navbar fixed-top navbar-expand-lg navbar-dark  fixed-top bg-color-main" style="background-color:#1d2330">
+
+ <section class="header-box fixed-bar">
+      <span style="font-size:30px;cursor:pointer; color: rgb(255, 000, 155); float: left;" onclick="openNav()">☰ </span>&nbsp;&nbsp;&nbsp;  
+      <a class="brand" href="index.php"><img src="images/logo1.png" width="184" height="61" alt="Logo"></a>
+    <h2 class="brand"><a href="index.php"><span style="color: black"> Kinmat Technologies</span> <span style="color: #0099FF">Limited | Blog</span> </a></h2>
+        
+         
+</section>
+    
+</nav>
+
+</body>
+</html>
